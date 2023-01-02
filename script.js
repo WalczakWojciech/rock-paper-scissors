d3ba9b28c1aa2f362136aa7895464bdd81b66bd3
WalczakWojciech <wojciechkrzysztofwalczak@gmail.com>
2023-01-02 13:08:27 +0100

    playerSelection insensitve to variation of input

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 function getComputerChoice(){
-
+    const arrayComp = ["Rock", "Paper", "Scissors"];
+    let computerSelection = arrayComp[Math.floor(Math.random()*arrayComp.length)];
+    return computerSelection;
+}
+function getUserChoice(){
+    // return playerSelection by buttons
 }
 
 function playRound(playerSelection, computerSelection){
-    //your code here
+    const str = playerSelection.toLowerCase();
+    const str1 = str.charAt(0).toUpperCase() + str.slice(1);
+    console.log(str1);
 }
 
-const playerSelection = "rock";
+const playerSelection = "roCk";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+getComputerChoice();
+console.log(computerSelection);
+playRound(playerSelection, computerSelection);
+
+
