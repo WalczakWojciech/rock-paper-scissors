388bd8933217eb797396cd87320e21f3c33c4c87
WalczakWojciech <wojciechkrzysztofwalczak@gmail.com>
2023-01-02 13:42:25 +0100

    Function compare playerSelection and computerSelection

--- script.js
+++ script.js
@@ -8,17 +8,32 @@ function getUserChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    const str = playerSelection.toLowerCase();
-    const str1 = str.charAt(0).toUpperCase() + str.slice(1);
-    console.log(str1);
-}
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 
+    if (playerSelection == "Paper" && computerSelection == "Rock"){
+        console.log("You win! Paper beats Rock");
+        } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
+            console.log("You lose! Scissors beats Paper");
+            } else if (playerSelection == "Rock" && computerSelection == "Paper"){
+                console.log("You lose! Paper beats Rock");
+            } else if (playerSelection == "Rock" && computerSelection == "Scissors"){
+                console.log("You win! Rock beats Scissors");
+                } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
+                console.log("You win! Scissors beats Paper");
+                } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
+                console.log("You lose! Rock beats Scissors");
+            } else {
+                console.log("Draw!");
+            };
+}
 const playerSelection = "roCk";
 const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
 
 getComputerChoice();
-console.log(computerSelection);
+console.log(getComputerChoice());
+//console.log(playerSelection);
 playRound(playerSelection, computerSelection);
 
 
